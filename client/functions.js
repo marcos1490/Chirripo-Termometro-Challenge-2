d64a3dfaf00d27a559b85da541071492f7a543f7
Marco <msalazar@Marcos-MacBook-Pro-2.local>
2013-11-10 11:04:02 -0600

    Add server side part.

--- client/functions.js
+++ client/functions.js
@@ -1,15 +1,49 @@
 $(document).ready(function() {
+  var socket = io.connect('http://localhost:9000');
 
-	$( "#selectable" ).selectable({
-  		selected: function( event, ui ) {
-  			var color = $('.color').val();
-  			color = "#"+color;
-  			var selected = ui.selected;
-  			
-  			$(selected).removeClass('ui-selected');
-  			$(selected).css("background-color" , color);
+  socket.on('newClient', function(data) {
+    console.log(data);
+    if (data.currentGrid.length === 100) {
+      console.log(data.currentGrid);
+      var childrens = $("#selectable").children();
 
-  		}
-	});
+      for (var i = 0; i < data.currentGrid.length; i++) {
+        $(childrens[i]).css('background-color', data.currentGrid[i]);
+      }
+    }
+
+  });
+
+  socket.on('colorUpdated', function(data) {
+    var childrens = $("#selectable").children();
+    for (var i = 0; i < data.currentGrid.length; i++) {
+      $(childrens[i]).css('background-color', data.currentGrid[i]);
+    }
+
+  });
+
+  $("#selectable").selectable({
+    selected: function(event, ui) {
+      var color = $('.color').val();
+      color = "#" + color;
+      var selected = ui.selected;
+
+      $(selected).removeClass('ui-selected');
+      $(selected).css("background-color", color);
+
+
+      var childrens = $("#selectable").children();
+      var colors = [];
+      for (var i = 0; i < childrens.length; i++) {
+        colors.push($(childrens[i]).css('background-color'));
+
+      }
+
+      socket.emit('colorChanged', {
+        newColors: colors
+      });
+
+    }
+  });
 
 });
---
+++ server/server.js
@@ -0,0 +1,17 @@
+var io = require('socket.io').listen(9000);
+ var colors = []; 
+
+io.sockets.on('connection', function (socket) {
+  
+  socket.emit('newClient', { currentGrid: colors });
+  
+  socket.on('colorChanged', function (data) {
+   
+    colors = data.newColors;
+    socket.emit('colorUpdated', { currentGrid: colors });
+  });
+
+console.log("New client connected");
+
+});
+
